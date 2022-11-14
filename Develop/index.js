7b3fd6e6fa91829ac6223fd47117a1c562b430a6
kennacapri <kennac28@gmail.com>
2022-11-14 12:41:33 -0700

    added type, name, input and input to each section of READme file (title, description, usage, etc)

--- Develop/index.js
+++ Develop/index.js
@@ -69,27 +69,25 @@ const questions = [
         }
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'what is the title of your README?',
-        validate: (titleInput) => {
-            if (licenseInput) {
-                return true;
-            } else {
-                console.log('Please enter a title for your README!');
-            return false;
-            }
-        }
+        message: 'please select a license',
+        choices: [
+            'MIT License',
+            'Apache License 2.0',
+            'BSD 2-Clause "Simplified" License'
+        ]
+
     },
     {
         type: 'input',
         name: 'contributing',
-        message: 'what is the title of your README?',
-        validate: (titleInput) => {
-            if (titleInput) {
+        message: 'who are the contributors of your project?',
+        validate: (contInput) => {
+            if (contInput) {
                 return true;
             } else {
-                console.log('Please enter a title for your README!');
+                console.log('Please add any contributions!');
             return false;
             }
         }
@@ -97,12 +95,12 @@ const questions = [
     {
         type: 'input',
         name: 'tests',
-        message: 'what is the title of your README?',
-        validate: (titleInput) => {
-            if (titleInput) {
+        message: 'what are the test instructions if you have any?',
+        validate: (testInput) => {
+            if (testInput) {
                 return true;
             } else {
-                console.log('Please enter a title for your README!');
+                console.log('Please add test instructions inf applicable!');
             return false;
             }
         }
@@ -110,18 +108,17 @@ const questions = [
     {
         type: 'input',
         name: 'questions',
-        message: 'what is the title of your README?',
-        validate: (titleInput) => {
-            if (titleInput) {
+        message: 'what are possible questions if you have any?',
+        validate: (questionsInput) => {
+            if (questionsInput) {
                 return true;
             } else {
-                console.log('Please enter a title for your README!');
+                console.log('Please add applicable questions to your README!');
             return false;
             }
         }
     },
 
-];
 ];
 
 // TODO: Create a function to write README file
