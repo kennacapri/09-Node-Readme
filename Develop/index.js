fb30fd781a650c7511857403f9c30ecfbe687cef
kennacapri <kennac28@gmail.com>
2022-11-14 16:13:56 -0700

    added function to display badges for each license

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,20 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const createREADME = (title, description, contents, installation, usage, license, contributing, tests, questions ) => {
-const README = 
-`#${title}
+const createREADME = (
+  title,
+  description,
+  contents,
+  installation,
+  usage,
+  license,
+  contributing,
+  tests,
+  questions
+) => {
+  const README = 
+  `#${title}
 
 ##Description
 ${description}
@@ -32,142 +42,165 @@ ${tests}
 ${questions}
 `;
 
-}
+return README;
+};
 
-const questions = [
+inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "what is the title of your README?",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter a title for your README!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "write a short description of your project",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("Please add a description for your README!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "contents",
+      message: "table of contents is only useful if README file is long",
+      validate: (contentInput) => {
+        if (contentInput) {
+          return true;
+        } else {
+          console.log(
+            "Please add table of contents if your README file is long!"
+          );
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "how to install your application?",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter an installation guide!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How are you going to use your application?",
+      validate: (usageInput) => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log(
+            "Please add info on how you are going to use your project!"
+          );
+          return false;
+        }
+      },
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "please select a license",
+      choices: [
+        "MIT License",
+        "Apache License 2.0",
+        'BSD 2-Clause "Simplified" License',
+      ],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "who are the contributors of your project?",
+      validate: (contInput) => {
+        if (contInput) {
+          return true;
+        } else {
+          console.log("Please add any contributions!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "what are the test instructions if you have any?",
+      validate: (testInput) => {
+        if (testInput) {
+          return true;
+        } else {
+          console.log("Please add test instructions inf applicable!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "what are possible questions if you have any?",
+      validate: (questionsInput) => {
+        if (questionsInput) {
+          return true;
+        } else {
+          console.log("Please add applicable questions to your README!");
+          return false;
+        }
+      },
+    },
+  ])
+  .then((answers) => {
+    const {
+      title,
+      description,
+      contents,
+      installation,
+      usage,
+      license,
+      contributing,
+      tests,
+      questions,
+    } = answers;
 
-    {
-        type: 'input',
-        name: 'title',
-        message: 'what is the title of your README?',
-        validate: (titleInput) => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter a title for your README!');
-            return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'write a short description of your project',
-        validate: (descriptionInput) => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('Please add a description for your README!');
-            return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'contents',
-        message: 'table of contents is only useful if README file is long',
-        validate: (contentInput) => {
-            if (contentInput) {
-                return true;
-            } else {
-                console.log('Please add table of contents if your README file is long!');
-            return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'how to install your application?',
-        validate: (titleInput) => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter an installation guide!');
-            return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How are you going to use your application?',
-        validate: (usageInput) => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please add info on how you are going to use your project!');
-            return false;
-            }
-        }
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'please select a license',
-        choices: [
-            'MIT License',
-            'Apache License 2.0',
-            'BSD 2-Clause "Simplified" License'
-        ]
+    console.log(answers);
+    fs.writeFile(
+      "README.md",
+      createREADME(
+        title,
+        description,
+        contents,
+        installation,
+        usage,
+        license,
+        contributing,
+        tests,
+        questions),
+      (error) => {
+        if (error) throw error;
+      }
+    );
+  })
+  .catch ((error) => {
+    if (error.isTtyError) {
+    } else {
+    }
+  });
 
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'who are the contributors of your project?',
-        validate: (contInput) => {
-            if (contInput) {
-                return true;
-            } else {
-                console.log('Please add any contributions!');
-            return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'what are the test instructions if you have any?',
-        validate: (testInput) => {
-            if (testInput) {
-                return true;
-            } else {
-                console.log('Please add test instructions inf applicable!');
-            return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'what are possible questions if you have any?',
-        validate: (questionsInput) => {
-            if (questionsInput) {
-                return true;
-            } else {
-                console.log('Please add applicable questions to your README!');
-            return false;
-            }
-        }
-    },
 
-];
-module.exports = questions;
 
-// TODO: Create a function to write README file
-function linkFile(fileName, data) {
-    const fileData = generateMarkdown(data);
-    fs.linkFile(fileName, fileData, (error) => {});
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then((answers) => linkFile('README.,md', answers));
-}
-
-        
-
-// Function call to initialize app
-init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,18 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+  let licenseType = license.license;
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'Apache License 2.0') {
+    yourLicense = `![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)` 
+  } else if (licenseType === "BSD 2-Clause 'Simplified' License") {
+  yourLicense  = `![License: BSD License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+} else {
+  license.license = "N/A"
+}
+return yourLicense;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,8 +25,21 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  renderLicenseBadge(license)
+`
 }
+//index.js file
+const questions = [
+  {
+      type: "list",
+      name: "license",
+      message: "please select a license",
+      choices: [
+        "MIT License",
+        "Apache License 2.0",
+        'BSD 2-Clause "Simplified" License',
+      ],
+    }
+]
 
 module.exports = generateMarkdown;
