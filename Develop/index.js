51f00fe513e91fee054ae26cffff8378200c7ab3
kennacapri <kennac28@gmail.com>
2022-11-14 12:19:55 -0700

    moved const questions to index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,128 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'what is the title of your README?',
+        validate: (titleInput) => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your README!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'write a short description of your project',
+        validate: (descriptionInput) => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please add a description for your README!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: 'only useful if README file is long',
+        validate: (contentInput) => {
+            if (contentInput) {
+                return true;
+            } else {
+                console.log('Please add table of contents if your README file is long!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'what is the title of your README?',
+        validate: (titleInput) => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter an installation guide!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'what is the title of your README?',
+        validate: (usageInput) => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please add info on how you are going to use your project!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'what is the title of your README?',
+        validate: (titleInput) => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your README!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'what is the title of your README?',
+        validate: (titleInput) => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your README!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'what is the title of your README?',
+        validate: (titleInput) => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your README!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'what is the title of your README?',
+        validate: (titleInput) => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your README!');
+            return false;
+            }
+        }
+    },
+
+];
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
